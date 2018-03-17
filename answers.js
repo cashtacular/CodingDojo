7fcf11c26b5e7c79094a3be309b16f66a384a6e1
cashtacular <36427693+cashtacular@users.noreply.github.com>
2018-03-17 14:59:44 -0700

    Create answers.js

---
+++ answers.js
@@ -0,0 +1,37 @@
+
+<!DOCTYPEHTML>
+<html>
+<head></head>
+<body></body>
+	<script type="text/javascript">
+//setting and swapping
+var myName= 'Cash';
+
+var myNumber= 42;
+
+var temp=myName;
+
+myName=myNumber;
+
+myNumber=temp;
+console.log(myName,myNumber);
+
+
+//print and count
+var log=0;
+for(var i=512; i<4097; i++){
+	if(i%5===0){
+		console.log(i);
+		log+=1; 
+
+	}
+
+}
+console.log(log);
+
+</script>
+
+</html>
+
+
+//
